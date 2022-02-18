24e7f8b26c28d4a429902a6e5bd61ce01763e549
dmrudy17 <dmr136@zips.uakron.edu>
2022-02-17 20:42:27 -0500

    Setup Firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import * as firebase from "firebase/app";
 
-Vue.config.productionTip = false
+const firebaseConfig = {
+    apiKey: "AIzaSyDlW80QsSNWI0QeTP02F1oRuSkDBF6S_Z4",
+    authDomain: "litecount-5fba6.firebaseapp.com",
+    projectId: "litecount-5fba6",
+    storageBucket: "litecount-5fba6.appspot.com",
+    messagingSenderId: "1095864396718",
+    appId: "1:1095864396718:web:dce04920df42396f14f63f",
+    measurementId: "G-HKS5CKY6KG",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
