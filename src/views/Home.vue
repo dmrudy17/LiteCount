2a7819dad2fa71df5e56c6b8eb2db3e4d458a5d2
sgun7 <60078854+sgun7@users.noreply.github.com>
2022-02-19 16:44:48 -0500

    Update Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,41 @@
 <template>
-  <hello-world />
+    <v-container class ="my-5">
+
+      <v-layout row wrap>
+          <v-flex xs12 md12>
+              <home-page-carousel></home-page-carousel>
+          </v-flex>
+          <v-spacer class="spacer"></v-spacer>
+          <v-flex xs12 md6>
+            <image-card-1 title="Join us!" action="Sign Up" paragraph="We strive to be source of inventory managment for small companies" :imageSource="require('@/assets/networking-1024x512.png')"></image-card-1>
+          </v-flex>
+          <v-flex xs12 md6>
+            <image-card-1 title="Analytics" action="Read More" paragraph="Check out how fast we are growing by analyzing our database" :imageSource="require('@/assets/Analytics-Image.jpg')"></image-card-1>
+          </v-flex>
+      </v-layout>
+
+      <v-layout row wrap>
+         
+      </v-layout>
+
+    </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import ImageCard1 from '../components/ImageCard1'
+  import HomePageCarousel from '../components/HomePageCarousel'
 
   export default {
-    name: 'Home',
+    name: 'HomeView',
 
     components: {
-      HelloWorld,
+      ImageCard1,
+      HomePageCarousel
     },
   }
 </script>
+<style scoped>
+.spacer{
+  height: 600px;
+}
+</style>
