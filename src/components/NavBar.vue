5d4d193bec9046daa578560b62790ed4648055b2
dmrudy17 <dmr136@zips.uakron.edu>
2022-02-22 10:13:24 -0500

    Adjust color and elevation of navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,10 @@
 <template>
   <nav>
-    <v-app-bar fixed>
+    <v-app-bar
+      fixed
+      elevate-on-scroll
+      color=#F7FBFE
+    >
       <v-toolbar-title>
         <span class="font-weight-bold blue--text text--darken-4">Lite</span>
         <span class="blue--text text--darken-4">Count</span>
