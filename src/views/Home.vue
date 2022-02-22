53267a59eac649d7dfc711780707d31574ed11a3
dmrudy17 <dmr136@zips.uakron.edu>
2022-02-22 10:03:23 -0500

    Adjust spacing between carousel and navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
 
       <v-layout row wrap>
           <v-flex xs12 md12>
+              <v-spacer class="spacer-2"></v-spacer>
               <home-page-carousel></home-page-carousel>
           </v-flex>
           <v-spacer class="spacer"></v-spacer>
@@ -37,5 +38,8 @@
 <style scoped>
 .spacer{
   height: 600px;
+}
+.spacer-2{
+  height: 75px;
 }
 </style>
