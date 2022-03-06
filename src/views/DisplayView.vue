fa5f764438bba08dcfd289f5e1ab8c938a0e347c
sgun7 <60078854+sgun7@users.noreply.github.com>
2022-03-06 16:09:13 -0500

    Add dropdown for item numbers

--- src/views/DisplayView.vue
+++ src/views/DisplayView.vue
@@ -2,6 +2,13 @@
 <div>
     <v-spacer class="spacer"></v-spacer>
     <h1>Display View</h1>
+    <v-autocomplete
+        @click="dropDown"
+        :items="this.documents"
+        color="white"
+        item-text="name"
+        label="Item Number"
+    ></v-autocomplete>
 </div>
 </template>
 
@@ -11,6 +18,9 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 export default {
+      data: () => ({
+          documents: []
+    }),
     beforeMount(){
         firebase.auth().onAuthStateChanged((user) => {
             if(!user)
@@ -18,7 +28,20 @@ export default {
                 this.$router.push('/login');
             }
         })
+    },
+    methods:
+    {
+        async dropDown()
+        {
+            const snapshot = await firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").get()
+            snapshot.forEach(doc => {
+                const pulledVal = doc.id;
+                this.documents.push(pulledVal);
+            })
+        }
+
     }
+
 }
 </script>
 
