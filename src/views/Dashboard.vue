036a4f9b0460c4c3388482937255c5c58aa5990f
dmrudy17 <dmr136@zips.uakron.edu>
2022-03-06 09:19:02 -0500

    Create dashboard page

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+     <v-spacer class="spacer"></v-spacer>
+    <h1>This is a dashboard page</h1>
+  </div>
+</template>
+
+<style scoped>
+.spacer{
+  height: 100px;
+}
+</style>
