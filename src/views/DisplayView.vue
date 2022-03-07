c85e36263cec827d2f67e91dc840d75251bc022e
sgun7 <60078854+sgun7@users.noreply.github.com>
2022-03-07 17:39:24 -0500

    Display values properly

--- src/views/DisplayView.vue
+++ src/views/DisplayView.vue
@@ -3,23 +3,28 @@
     <v-spacer class="spacer"></v-spacer>
     <h1>Display View</h1>
     <v-autocomplete
-        @click="dropDown"
+        v-model='selectedId'
+        @change="loadValues"
         :items="this.documents"
         color="white"
-        item-text="name"
         label="Item Number"
     ></v-autocomplete>
+    {{dataObject["Item Name"]}}
+    <br>
+    {{dataObject.Quantity}}
 </div>
 </template>
 
 <script>
+/* eslint-disable */
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-
 export default {
       data: () => ({
-          documents: []
+          documents: [],
+          selectedId: '',
+          dataObject: '',
     }),
     beforeMount(){
         firebase.auth().onAuthStateChanged((user) => {
@@ -29,18 +34,43 @@ export default {
             }
         })
     },
-    methods:
+    async mounted()
     {
-        async dropDown()
-        {
-            const snapshot = await firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").get()
+        const snapshot = await firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").get()
             snapshot.forEach(doc => {
                 const pulledVal = doc.id;
                 this.documents.push(pulledVal);
             })
+    },
+    methods:
+    {
+       loadValues(e)
+        {
+            // firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").where()
+            // .then((value) => 
+            // console.log(value.data["Quantity"]));
+            console.log(e)
+            const db = firebase.firestore()
+            const articleRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(e)
+                
+            articleRef.get().then(articleDoc => {
+                if(articleDoc.exists)
+                {
+                    const article = articleDoc.data()
+                    this.dataObject = article
+                }
+            }) 
+            console.log(this.dataObject)
+                // .get()
+                //     .then(querySnapshot => {
+                //         querySnapshot.forEach((doc) => {
+                //             var firstName = doc.get("Item Name");
+                //             console.log(firstName)
+                //         });
+                //     })
         }
-
     }
+
 
 }
 </script>
