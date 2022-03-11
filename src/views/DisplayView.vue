d4d413a8330a59538375cede5bbae4c0a023d590
Quang Ngo <tsubasquang@gmail.com>
2022-03-10 19:57:37 -0500

    Add files

--- src/views/DisplayView.vue
+++ src/views/DisplayView.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+/* eslint-disable */
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
@@ -23,6 +24,42 @@ export default {
                 this.$router.push('/login');
             }
         })
+    },
+    async mounted()
+    {
+        const snapshot = await firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").get()
+            snapshot.forEach(doc => {
+                const pulledVal = doc.id;
+                this.documents.push(pulledVal);
+            })
+    },
+    methods:
+    {
+       loadValues(e)
+        {
+            // firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").where()
+            // .then((value) => 
+            // console.log(value.data["Quantity"]));
+            console.log(e)
+            const db = firebase.firestore()
+            const articleRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(e)
+                
+            articleRef.get().then(articleDoc => {
+                if(articleDoc.exists)
+                {
+                    const article = articleDoc.data()
+                    this.dataObject = article
+                }
+            }) 
+            console.log(this.dataObject)
+                // .get()
+                //     .then(querySnapshot => {
+                //         querySnapshot.forEach((doc) => {
+                //             var firstName = doc.get("Item Name");
+                //             console.log(firstName)
+                //         });
+                //     })
+        }
     }
 }
 </script>
