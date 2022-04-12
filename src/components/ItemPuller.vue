fd35949088aa2d588010363df91a468689565048
dmrudy17 <dmr136@zips.uakron.edu>
2022-04-12 00:53:47 -0400

    Adjust formatting and add admin-only overwrite button

--- src/components/ItemPuller.vue
+++ src/components/ItemPuller.vue
@@ -28,11 +28,14 @@
 
     <v-layout justify-center>
         <v-card-actions>
-            <v-btn  color="primary" @click="updateQuantity(); addMap();">
-            <span>Increment</span>
+            <v-btn class="mr-6" color="primary" @click="updateQuantity(); addMap();">
+              <span>Increment</span>
             </v-btn>
-            <v-btn  color="secondary" @click="decrementQuantity">
-            <span>Decrement</span>
+            <v-btn class="mr-6" color="secondary" @click="decrementQuantity">
+              <span>Decrement</span>
+            </v-btn>
+            <v-btn v-if="isAdminUser" color="error" @click="overwriteQuantity()">
+              <span>Overwrite Quantity</span>
             </v-btn>
         </v-card-actions>
     </v-layout>
@@ -46,102 +49,124 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 export default {
-    data() {
-          return {
-          documents: [],
-          selectedId: '',
-          dataObject: 0,
-          updatedValue: 0,
-          collectionName: this.$route.params.documentId,
-          nameOfDocument: '',
-          userId: '',
-          fullName: '',
-          }
-    },
-    beforeMount(){
-        firebase.auth().onAuthStateChanged((user) => {
-            if(user)
-            {
-              this.userId = user.uid
+  data() {
+    return {
+      documents: [],
+      selectedId: '',
+      dataObject: 0,
+      updatedValue: 0,
+      collectionName: this.$route.params.documentId,
+      nameOfDocument: '',
+      userId: '',
+      fullName: '',
+      isAdminUser: false,
+    }
+  },
+  beforeMount(){
+    const dbStore = firebase.firestore();
 
-            var userName = firebase.firestore().collection('Clients').doc("Litehouse").collection("Users").doc(user.uid)
-            userName.get().then(articleDoc => {
-            if(articleDoc.exists)
-            {
-                const article = articleDoc.data()
-                this.fullName = article.name
-            }
-            })
-            }
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.userId = user.uid
+
+        var userName = dbStore.collection('Clients').doc("Litehouse").collection("Users").doc(user.uid)
+        userName.get().then(articleDoc => {
+          if(articleDoc.exists) {
+              const article = articleDoc.data()
+              this.fullName = article.name
+          }
         })
 
+        dbStore
+        .collection("Clients")
+        .doc("Litehouse")
+        .collection("Users")
+        .where('isAdmin', '==', true)
+        .get()
+        .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            const adminUser = querySnapshot.docs[0].data();
+            if (adminUser.email == user.email) {
+              this.isAdminUser = true;
+            }
+          } else {
+            console.log("No admin found");
+          }
+        })
+      }
+    })
   },
-    async mounted()
-    {   
-        const snapshot = await firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").get()
-            snapshot.forEach(doc => {
-                const pulledVal = doc.id;
-                this.documents.push(pulledVal);
-            })
-        
+  async mounted()
+  {   
+    const snapshot = await firebase.firestore().collection('Clients').doc("Litehouse").collection("Items").get()
+        snapshot.forEach(doc => {
+            const pulledVal = doc.id;
+            this.documents.push(pulledVal);
+        })
+      
+  },
+  methods:
+  {
+    loadValues(e) {
+      this.nameOfDocument = e;
+      const db = firebase.firestore()
+      const articleRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(e)
+          
+      articleRef.get().then(articleDoc => {
+          if(articleDoc.exists)
+          {
+              const article = articleDoc.data()
+              this.dataObject = article
+          }
+      }) 
     },
-    methods:
-    {
-       loadValues(e)
-        {
-            this.nameOfDocument = e;
-            const db = firebase.firestore()
-            const articleRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(e)
-                
-            articleRef.get().then(articleDoc => {
-                if(articleDoc.exists)
-                {
-                    const article = articleDoc.data()
-                    this.dataObject = article
-                }
-            }) 
-        },
-        updateQuantity()
-        {
-            const db = firebase.firestore();
-            const increment = firebase.firestore.FieldValue.increment(this.updatedValue);
-            const newRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.nameOfDocument);
-            newRef.update({Quantity:increment})
+    updateQuantity() {
+      const db = firebase.firestore();
+      const increment = firebase.firestore.FieldValue.increment(this.updatedValue);
+      const newRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.nameOfDocument);
+      newRef.update({Quantity:increment})
 
-            //Update on card
-            var number = parseInt(this.updatedValue)
-            this.dataObject.Quantity = this.dataObject.Quantity + number;
+      //Update on card
+      var number = parseInt(this.updatedValue)
+      this.dataObject.Quantity = this.dataObject.Quantity + number;
+    },
+    async addMap() {
+      const db = firebase.firestore();
+      console.log(this.fullName)
+      var itemRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.selectedId);
+      var time = new Date().toLocaleDateString("en-US");
+      var logObject = {
+          "User" : this.fullName,
+          "Quantity": this.dataObject.Quantity,
+          "Date": time, 
+      }
+      itemRef.update({
+          "logs": firebase.firestore.FieldValue.arrayUnion(logObject)
+      })
+    },
+    decrementQuantity() {
+      const db = firebase.firestore();
+      const decrement = firebase.firestore.FieldValue.increment(-1 * this.updatedValue);
+      const newRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.nameOfDocument);
+      newRef.update({Quantity:decrement})
 
-        },
-        async addMap()
-        {
-            const db = firebase.firestore();
-            console.log(this.fullName)
-            var itemRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.selectedId);
-            var time = new Date().toLocaleDateString("en-US");
-            var logObject = {
-                "User" : this.fullName,
-                "Quantity": this.dataObject.Quantity,
-                "Date": time, 
-            }
-            itemRef.update({
-                "logs": firebase.firestore.FieldValue.arrayUnion(logObject)
-            }
-            )
-        },
-        decrementQuantity()
-        {
-            console.log(this.updatedValue);
-            const db = firebase.firestore();
-            const decrement = firebase.firestore.FieldValue.increment(-1 * this.updatedValue);
-            const newRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.nameOfDocument);
-            newRef.update({Quantity:decrement})
+      //Update on card
+      var number = parseInt(this.updatedValue)
+      this.dataObject.Quantity = this.dataObject.Quantity - number;
+    },
+    overwriteQuantity() {
+      if (confirm("Are you sure you would like to overwrite this quantity?")) {
+        const db = firebase.firestore();
+        const overwrite = firebase.firestore.FieldValue.increment(this.updatedValue);
+        const newRef = db.collection('Clients').doc("Litehouse").collection("Items").doc(this.nameOfDocument);
+        newRef.set({Quantity:overwrite})
 
-            //Update on card
-            var number = parseInt(this.updatedValue)
-            this.dataObject.Quantity = this.dataObject.Quantity - number;
-        }
+        //Update on card
+        var number = parseInt(this.updatedValue)
+        this.dataObject.Quantity = number;
+      }
     }
+  }
 
 
 }
